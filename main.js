c7e79e20bb14bf62fc4077454702495b65d7ff4d
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-09 17:14:15 +0200

    add milestone 2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 
 const formEl = document.querySelector('form')
+const yourTicketEl = document.querySelector('.ticket')
 
 formEl.addEventListener('submit', function (e) {
     e.preventDefault()
@@ -8,6 +9,41 @@ formEl.addEventListener('submit', function (e) {
     const kms = e.target.kms.value
     const age = e.target.age.value
     console.log(nameSurname, kms, age);
+
+    let price
+
+    price = kms * 0.21;
+    finalPrice = price.toFixed(2);
+    if (age.value = 'under 18') {
+        price = price - (price / 100 * 20)
+    } else if (age.value = 'over 65') {
+        price = price - (price / 100 * 40)
+    } else {
+        price = kms * 0.21
+    }
+
+    const carrozza = Math.floor(Math.random() * 10) + 1;
+    const seat = Math.floor(Math.random() * 25) + 1;
+    const codeTicket = Math.floor(Math.random() * 90000) + 10000;
+
+    const memberEl = `
+        <div class="row d-flex">
+            <h5 class="col-4 py-3">NOME PASSEGGERO</h5>
+            <h5 class="col-2 py-3">CARROZZA</h5>
+            <h5 class="col-2 py-3">POSTO</h5>
+            <h5 class="col-2 py-3">CODICE BIGLIETTO</h5>
+            <h5 class="col-2 py-3">PREZZO</h5>
+        </div>
+        <div class="row d-flex">
+            <div class="col-4 py-2"> ${nameSurname} </div>
+            <div class="col-2 py-2"> ${carrozza} </div>
+            <div class="col-2 py-2"> ${seat} </div>
+            <div class="col-2 py-2"> ${codeTicket} </div>
+            <div class="col-2 py-2"> ${finalPrice} </div>
+        </div>
+    `
+
+    yourTicketEl.insertAdjacentHTML('beforeend', memberEl)
 })
 
 
@@ -16,18 +52,3 @@ formEl.addEventListener('submit', function (e) {
 
 
 
-
-/* price = kms * 0.21;
-finalPrice = price.toFixed(2);
-if (age < 18) {
-    price = price - (price / 100 * 20) 
-    message = `Il costo del tuo biglietto è di ${finalPrice} sconto applicato del 20%`
-} else if (age > 65) {
-    price = price - (price / 100 * 40)
-    message = `Il costo del tuo biglietto è di ${finalPrice} sconto applicato del 40%`
-} else {
-    price = kms * 0.21
-    message = `Il costo del tuo biglietto è di ${finalPrice}`
-}
-
-console.log(finalPrice, message) */
