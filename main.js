8aa02ddc983863e45930c3dbed7d962567c718c0
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-09 15:52:13 +0200

    complete milestone 1

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 
+const formEl = document.querySelector('form')
+
+formEl.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    const kms = e.target.kms.value
+    console.log(kms);
+})
 
 
 
@@ -7,6 +15,17 @@
 
 
 
+/* price = kms * 0.21;
+finalPrice = price.toFixed(2);
+if (age < 18) {
+    price = price - (price / 100 * 20) 
+    message = `Il costo del tuo biglietto è di ${finalPrice} sconto applicato del 20%`
+} else if (age > 65) {
+    price = price - (price / 100 * 40)
+    message = `Il costo del tuo biglietto è di ${finalPrice} sconto applicato del 40%`
+} else {
+    price = kms * 0.21
+    message = `Il costo del tuo biglietto è di ${finalPrice}`
+}
 
-
-
+console.log(finalPrice, message) */
